cba9442a1ecbd45642c14d4abf410937a29e3b9d
Aaron Shim <aaron.k.shim@gmail.com>
2017-06-02 22:22:51 -0700

    fonts.css rename

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /* WEBPACK ENTRYPOINT 
    (All our Elm code, CSS, etc. get aggregated here.)
 */
-require('./css/Fonts.css');
+require('./css/fonts.css');
 require('normalize.css');
 require('milligram');
 require('./css/Stylesheets.elm');
