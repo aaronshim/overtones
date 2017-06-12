af42a0583854d5b62d50443eabc90326b96512a1
Aaron Shim <aaron.k.shim@gmail.com>
2017-06-11 23:42:11 -0700

    actually adding example tests so our CI doesn't crash

--- tests/Example.elm
+++ tests/Example.elm
@@ -7,4 +7,13 @@ import Fuzz exposing (list, int, string)
 
 suite : Test
 suite =
-    todo "Implement our first test. See http://package.elm-lang.org/packages/elm-community/elm-test/latest for how to do this!"
+    describe "Sample module"
+        [ describe "Addition group laws"
+            [ fuzz2 int int "Commutation" <|
+                \x y -> Expect.equal (x + y) (y + x)
+            , fuzz int "Identity" <|
+                \x -> Expect.equal (x + 0) x
+            , fuzz3 int int int "Associativity" <|
+                \x y z -> Expect.equal (x + (y + z)) ((x + y) + z)
+            ]
+        ]
