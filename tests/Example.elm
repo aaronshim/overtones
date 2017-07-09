f1045ebbcc5aa1bd53a3f2f3831a494b69f5e7a7
Aaron Shim <aaron.k.shim@gmail.com>
2017-07-08 21:21:31 -0700

    more comprehensive tests for the collections

--- tests/Example.elm
+++ tests/Example.elm
@@ -2,7 +2,7 @@ module Example exposing (..)
 
 import Test exposing (..)
 import Expect
-import Fuzz exposing (list, int, string, maybe, Fuzzer, conditional)
+import Fuzz exposing (list, int, string, maybe, Fuzzer, conditional, tuple)
 import Dict
 import Collection exposing (..)
 
@@ -24,14 +24,27 @@ notEmptyList fuzzer =
 -- Dealing with fuzzing a stream of manipulations on the Collection
 
 
-translateToAction : Maybe a -> (CollectionWithContext a b -> CollectionWithContext a b)
-translateToAction n =
+translateToInsertsAndRemoveLast : Maybe a -> (CollectionWithContext a b -> CollectionWithContext a b)
+translateToInsertsAndRemoveLast n =
     case n of
         Just n_ ->
             insert n_
 
         Nothing ->
             removeLastInserted
+
+
+translateToInsertsAndRemoveIndex : CollectionWithContext a b -> a -> Maybe Int -> (CollectionWithContext a b -> CollectionWithContext a b)
+translateToInsertsAndRemoveIndex collectionSoFar elemToInsert index =
+    case index of
+        Just index_ ->
+            if isValidIndex index_ collectionSoFar then
+                remove index_
+            else
+                removeLastInserted
+
+        Nothing ->
+            insert elemToInsert
 
 
 isJust : Maybe a -> Bool
@@ -44,14 +57,14 @@ isJust x =
             False
 
 
-numInserts : List (Maybe a) -> Int
-numInserts =
+numInsertsForInsertsAndRemoveLast : List (Maybe a) -> Int
+numInsertsForInsertsAndRemoveLast =
     List.length << List.filter isJust
 
 
-numRemoves : List (Maybe a) -> Int
-numRemoves =
-    List.length << List.filter (not << isJust)
+numInsertsForInsertsAndRemoveIndex : List ( a, Maybe Int ) -> Int
+numInsertsForInsertsAndRemoveIndex =
+    List.length << List.filter (\( _, indexToRemove ) -> not <| isJust indexToRemove)
 
 
 
@@ -61,6 +74,11 @@ numRemoves =
 contains : a -> CollectionWithContext a b -> Bool
 contains x collection =
     toDict collection |> Dict.values |> List.member x
+
+
+isValidIndex : Int -> CollectionWithContext a b -> Bool
+isValidIndex i collection =
+    toDict collection |> Dict.keys |> List.member i
 
 
 suite : Test
@@ -87,14 +105,41 @@ suite =
                 \xs ->
                     let
                         collectionAfterInsert =
-                            List.foldl (\f accm -> (translateToAction f) accm) (emptyCollectionWithContext NoContext) xs
+                            List.foldl (\f accm -> (translateToInsertsAndRemoveLast f) accm) (emptyCollectionWithContext NoContext) xs
                     in
-                        Expect.atMost (numInserts xs) (Dict.size <| toDict collectionAfterInsert)
+                        Expect.atMost (numInsertsForInsertsAndRemoveLast xs) (Dict.size <| toDict collectionAfterInsert)
             , fuzz (list (maybe int)) "Correct next index after multiple inserts and removeLast" <|
                 \xs ->
                     let
                         collectionAfterInsert =
-                            List.foldl (\f accm -> (translateToAction f) accm) (emptyCollectionWithContext NoContext) xs
+                            List.foldl (\f accm -> (translateToInsertsAndRemoveLast f) accm) (emptyCollectionWithContext NoContext) xs
+                    in
+                        -- nextIndex should not be present in the dict currently
+                        Expect.equal Nothing (Dict.get collectionAfterInsert.nextIndex (toDict collectionAfterInsert))
+            ]
+        , describe "Multiple inserts with Remove (index) and RemoveLast"
+            {- we are going to use a tuple fuzzer here because we need things in the shape [(a, Maybe Int)] because there should be
+               as many of the a as there are Maybe Int's since two of them combined together form a correct input to the fuzzing test.
+               (a is what gets inserted and Maybe Int is an index to remove or a Nothing to signify an insert)
+               (to clean it up, I guess we can write our own fuzzer? It's harder for this one that has a dependence on the current
+               state of the collection to make a valid command, though...)
+            -}
+            [ fuzz (list (tuple ( int, maybe int ))) "Correct size after multiple inserts and removeLast and remove (by index)" <|
+                \xs ->
+                    let
+                        collectionAfterInsert =
+                            List.foldl (\( toInsert, f ) accm -> (translateToInsertsAndRemoveIndex accm toInsert f) accm)
+                                (emptyCollectionWithContext NoContext)
+                                xs
+                    in
+                        Expect.atMost (numInsertsForInsertsAndRemoveIndex xs) (Dict.size <| toDict collectionAfterInsert)
+            , fuzz (list (tuple ( int, maybe int ))) "Correct next index after multiple inserts and removeLast and remove (by index)" <|
+                \xs ->
+                    let
+                        collectionAfterInsert =
+                            List.foldl (\( toInsert, f ) accm -> (translateToInsertsAndRemoveIndex accm toInsert f) accm)
+                                (emptyCollectionWithContext NoContext)
+                                xs
                     in
                         -- nextIndex should not be present in the dict currently
                         Expect.equal Nothing (Dict.get collectionAfterInsert.nextIndex (toDict collectionAfterInsert))
